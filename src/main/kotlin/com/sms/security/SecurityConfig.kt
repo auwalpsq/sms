0ab1e54b91373c0b5ffa10eeae6597687472f2f6
Auwal Usman <51510385+auwalpsq@users.noreply.github.com>
2025-07-14 12:56:34 +0930

    Update SecurityConfig.kt

--- src/main/kotlin/com/sms/security/SecurityConfig.kt
+++ src/main/kotlin/com/sms/security/SecurityConfig.kt
@@ -1,3 +1,5 @@
+package com.sms.security
+
 import com.sms.security.ui.view.LoginView
 import com.sms.services.MyUserDetailsManager
 import com.vaadin.flow.spring.security.VaadinWebSecurity
@@ -12,11 +14,40 @@ import org.springframework.security.crypto.password.PasswordEncoder
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfig : VaadinWebSecurity() {
+class SecurityConfig(
+    private val userDetailsManager: MyUserDetailsManager,
+    private val passwordEncoder: PasswordEncoder // now injected, not defined here!
+) : VaadinWebSecurity() {
 
     override fun configure(http: HttpSecurity) {
         super.configure(http)
 
+        http.authorizeHttpRequests { auth ->
+            auth.requestMatchers(
+                "/",
+                "/login",
+                "/public/**",
+                "/icons/**",
+                "/images/**",
+                "/VAADIN/**"
+            ).permitAll()
+        }
+
         setLoginView(http, LoginView::class.java)
+    }
+
+    @Bean
+    fun authenticationManager(http: HttpSecurity): AuthenticationManager {
+        val builder = http.getSharedObject(AuthenticationManagerBuilder::class.java)
+        builder
+            .userDetailsService(userDetailsManager)
+            .passwordEncoder(passwordEncoder)
+
+        return builder.build()
+    }
+
+    @Bean
+    fun userDetailsService(): UserDetailsService {
+        return userDetailsManager
     }
 }
