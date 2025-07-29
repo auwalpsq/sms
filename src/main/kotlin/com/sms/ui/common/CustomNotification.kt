47ad1b408cac697b50b21bf6f51bc284f4f44246
Auwal Usman <51510385+auwalpsq@users.noreply.github.com>
2025-07-29 10:33:31 +0100

    Notification Update

--- src/main/kotlin/com/sms/ui/common/CustomNotification.kt
+++ src/main/kotlin/com/sms/ui/common/CustomNotification.kt
@@ -1,19 +1,38 @@
 package com.sms.ui.common
 
+import com.vaadin.flow.component.html.Div
+import com.vaadin.flow.component.html.Span
 import com.vaadin.flow.component.notification.Notification
+import com.vaadin.flow.component.notification.NotificationVariant
+import com.vaadin.flow.component.orderedlayout.FlexComponent
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout
 
 fun showSuccess(message: String) {
-    Notification(message, 3000, Notification.Position.TOP_CENTER).apply {
-        addClassNames("success-notification")
-        //addThemeName("success-notification")
+    Notification().apply {
+        val text = Span(message)
+        val layout = HorizontalLayout(text)
+        layout.justifyContentMode = FlexComponent.JustifyContentMode.CENTER
+        //layout.alignItems = FlexComponent.Alignment.CENTER
+        layout.setWidthFull()
+        add(layout)
+        addThemeVariants(NotificationVariant.LUMO_SUCCESS)
+        position = Notification.Position.TOP_STRETCH
+        duration = 3000
         open()
     }
 }
 
 fun showError(message: String) {
-    Notification(message, 3000, Notification.Position.TOP_CENTER).apply {
-        addClassNames("error-notification")
-        //addThemeName("error-notification")
+    Notification().apply {
+        val text = Span(message)
+        val layout = HorizontalLayout(text)
+        layout.justifyContentMode = FlexComponent.JustifyContentMode.CENTER
+        //layout.alignItems = FlexComponent.Alignment.CENTER
+        layout.setWidthFull()
+        add(layout)
+        addThemeVariants(NotificationVariant.LUMO_ERROR)
+        position = Notification.Position.TOP_STRETCH
+        duration = 3000
         open()
     }
 }
